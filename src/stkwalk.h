1c4633ec32d93246c0228d34c9f1178b62ccda93
Gisle Vanem <gisle.vanem@gmail.com>
2017-01-25 15:55:42 +0000

    Removed '#error' if '_X86_' wasn't defined.

--- src/stkwalk.h
+++ src/stkwalk.h
@@ -12,10 +12,6 @@
 #ifndef __STACKWALKER_H__
 #define __STACKWALKER_H__
 
-#if !defined(_X86_)
-//  #error Only INTEL environments are supported!
-#endif
-
 extern BOOL  StackWalkInit (void);
 extern BOOL  StackWalkExit (void);
 extern char *StackWalkShow (HANDLE thread, CONTEXT *c);
