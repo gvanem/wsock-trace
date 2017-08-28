cfd2c148b6e16fdc9af01c82bc302c59c1717ff7
Gisle Vanem <gisle.vanem@gmail.com>
2017-08-28 10:11:49 +0000

    Fix typo.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -93,7 +93,7 @@ set MINGW64_BIN=.
 if %2. == x64. (
   set MINGW64_BIN=c:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\bin
   set MINGW64_BIN=c:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\bin
-  dir %MINGW64_BIN%\*gcc*.exe
+  dir %MING64_BIN%\*gcc*.exe
 )
 
 set PATH=%MINGW64_BIN%;c:\MinGW\bin;%PATH%
