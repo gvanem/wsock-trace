7c4304735afe2616c7c2e63def141f56d5427182
Gisle Vanem <gisle.vanem@gmail.com>
2018-01-01 19:24:28 +0000

    Use 'who_am_I = __FILE__()'.

--- src/wsock_trace_init.lua
+++ src/wsock_trace_init.lua
@@ -11,7 +11,7 @@ function __FILE__()
   return string.sub (src,2)
 end
 
-who_am_I = string.format ("%s\\%s", os.getenv("APPDATA"), "wsock_trace_init.lua")
+who_am_I = __FILE__()
 
-io.write (string.format("Hello from '%s': %s\n", __FILE__(), os_details()))
+io.write (string.format("Hello from '%s': %s\n", who_am_I, os_details()))
 io.write ("package.path[]: " .. package.path .. '.\n')
