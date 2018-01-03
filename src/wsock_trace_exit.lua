5a78d63bed886fa7a21e1dbedc6544487fdb9a3d
Gisle Vanem <gisle.vanem@gmail.com>
2018-01-03 10:18:58 +0000

    Test the 'ws.get_builder()' method. For now, just use either   require ("wsock_trace_x64") or   require ("wsock_trace")
    Until I figure out how/what module-name the .lua-scripts
    should use. It would be best to simply have
      require ("wsock_trace")

--- src/wsock_trace_exit.lua
+++ src/wsock_trace_exit.lua
@@ -2,6 +2,5 @@
 --
 
 who_am_I = __FILE__()
-io.write (string.format("  Bye from ~1%s~0\n", who_am_I))
--- ws.trace_puts (string.format("  Bye from ~1%s~0\n", who_am_I))
+ws.trace_puts (string.format("  Bye from ~1%s~0\n", who_am_I))
 
--- src/wsock_trace_init.lua
+++ src/wsock_trace_init.lua
@@ -12,30 +12,34 @@ end
 
 --- Try the MinGW base names first.
 
+--- if jit.arch == "x64" then
+---   ws_name = "wsock_trace_mw_x64"
+--- else
+---   ws_name = "wsock_trace_mw"
+--- end
+---
+--- if not package.loaded [ws_name] then
+---   ws = require (ws_name)
+--- else
+---   --- Then if 'ws == nil', try the MSVC base names.
+---
+---   if jit.arch == "x64" then
+---     ws_name = "wsock_trace_x64"
+---   else
+---     ws_name = "wsock_trace"
+---   end
+---   ws = require (ws_name)
+--- end
+
 if jit.arch == "x64" then
-  ws_name = "wsock_trace_mw_x64"
+  ws = require ("wsock_trace_x64")
 else
-  ws_name = "wsock_trace_mw"
-end
-
-if not package.loaded [ws_name] then
-  ws = require (ws_name)
-else
-  --- Then if 'ws == nil', try the MSVC base names.
-
-  if jit.arch == "x64" then
-    ws_name = "wsock_trace_x64"
-  else
-    ws_name = "wsock_trace"
-  end
-  ws = require (ws_name)
+  ws = require ("wsock_trace")
 end
 
 who_am_I = __FILE__()
 
 ws.trace_puts (string.format("  Hello from '~1%s~0': ~2%s~0.\n", who_am_I, os_details()))
 ws.trace_puts ("  ~1package.path[]: " .. package.path .. '.~0\n')
-
-if ws then
-  ws.trace_puts (string.format("  I am importing from '~1%s~0'\n", ws.get_dll_name()))
-endif
+ws.trace_puts (string.format("  I am importing from: ~1%s~0. ws.get_builder(): ~1%s~0\n",
+               ws.get_dll_name(), ws.get_builder()))
