c01d78bd26685732cfddb28f1fd397457cb8930e
Gisle Vanem <gisle.vanem@gmail.com>
2018-01-04 14:24:04 +0000

    Just playing with the Lua scripts.

--- src/wsock_trace_exit.lua
+++ src/wsock_trace_exit.lua
@@ -1,6 +1,10 @@
 -- wsock_trace_exit.lua
 --
 
-who_am_I = __FILE__()
-ws.trace_puts (string.format("  Bye from ~1%s~0\n", who_am_I))
+if ws == nil then
+  dofile ("wsock_trace_init.lua")
+end
+
+ws.trace_puts (string.format("  Bye from ~1%s~0 at line %d\n", __FILE__(), __LINE__()))
+
 
--- src/wsock_trace_init.lua
+++ src/wsock_trace_init.lua
@@ -10,6 +10,18 @@ function __FILE__()
   return string.sub (src,2)
 end
 
+function __LINE__()
+  return debug.getinfo(2,'l').currentline
+end
+
+function __FUNC__()
+  return debug.getinfo(2,'n').name
+end
+
+WSAStartup = function (arg)
+  io.write (string.format("Hello from %s().\n", __FUNC__()))
+end
+
 --- Try the MinGW base names first.
 
 --- if jit.arch == "x64" then
@@ -32,14 +44,46 @@ end
 --- end
 
 if jit.arch == "x64" then
-  ws = require ("wsock_trace_x64")
+  ws_name = "wsock_trace_x64"
 else
-  ws = require ("wsock_trace")
+  ws_name = "wsock_trace"
+end
+
+if package.loaded [ws_name] then
+  ws.trace_puts (string.format("  Package ~2%s~0 already loaded; ~1ws -> %p~0\n", ws_name, ws))
+else
+  ws = require (ws_name)
+end
+
+function trace_printf (fmt, ...)
+  ws.trace_puts ("\ntrace_printf(): ")
+  str = string.format (fmt, unpack(arg))
+  ws.trace_puts (str)
 end
 
 who_am_I = __FILE__()
 
-ws.trace_puts (string.format("  Hello from '~1%s~0': ~2%s~0.\n", who_am_I, os_details()))
-ws.trace_puts ("  ~1package.path[]: " .. package.path .. '.~0\n')
-ws.trace_puts (string.format("  I am importing from: ~1%s~0. ws.get_builder(): ~1%s~0\n",
-               ws.get_dll_name(), ws.get_builder()))
+ws.trace_puts (string.format("  ws.get_trace_level: ~1%d~0.\n", ws.get_trace_level()))
+
+if nil then
+  ws.trace_puts ("  ws.set_trace_level(0).\n")
+  ws.set_trace_level (0)
+end
+
+-- trace_printf ("Hello from: ~1%s~0.\n", who_am_I)
+
+-- ws.trace_printf ("Hello from: ~1%s~0.\nVersion:  ~1%s~0. Arg: %s, another arg: %s\n", who_am_I, os_details(), "10", "hello")
+-- ws.trace_printf ("Hello from: ~1%s~0.\n", who_am_I)
+
+ws.trace_puts (string.format("  Hello from:         ~1%s~0.\n", who_am_I))
+ws.trace_puts (string.format("  Version:            ~1%s~0.\n", os_details()))
+ws.trace_puts (string.format("  This is line:       ~1%d~0.\n", __LINE__()))
+
+ws.trace_puts ("  package.path[]:     ~1" .. package.path .. "~0.\n")
+ws.trace_puts ("  package.cpath[]:    ~1" .. package.cpath .. "~0.\n")
+
+ws.trace_puts (string.format("  I'm importing from: ~1%s~0. ws.get_builder(): ~1%s~0.\n",
+               ws.get_dll_full_name(), ws.get_builder()))
+
+ws.register_hook (WSAStartup, "2")
+WSAStartup()
