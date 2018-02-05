cbee59bc2a0c62b478c34c99df8a694f0c38b5d8
Gisle Vanem <gisle.vanem@gmail.com>
2018-02-05 16:26:36 +0000

    Add these "DNSBL" settings:   'drop_url', 'edrop_url' and 'dropv6_url'.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -11,53 +11,57 @@ exit /b 0
 :: The CPU agnostic init-stage.
 ::
 echo Generating %CD%\wsock_trace.appveyor...
-echo #                                               > wsock_trace.appveyor
-echo # This file was generated from %0.             >> wsock_trace.appveyor
-echo #                                              >> wsock_trace.appveyor
-echo [core]                                         >> wsock_trace.appveyor
-echo trace_level            = %%WSOCK_TRACE_LEVEL%% >> wsock_trace.appveyor
-echo trace_indent           = 2                     >> wsock_trace.appveyor
-echo trace_caller           = 1                     >> wsock_trace.appveyor
-echo trace_report           = %%WSOCK_TRACE_LEVEL%% >> wsock_trace.appveyor
-echo trace_time             = relative              >> wsock_trace.appveyor
-echo trace_max_len          = %%COLUMNS%%           >> wsock_trace.appveyor
-echo callee_level           = 1                     >> wsock_trace.appveyor
-echo cpp_demangle           = 1                     >> wsock_trace.appveyor
-echo short_errors           = 1                     >> wsock_trace.appveyor
-echo use_full_path          = 1                     >> wsock_trace.appveyor
-echo use_toolhlp32          = 1                     >> wsock_trace.appveyor
-echo dump_modules           = 0                     >> wsock_trace.appveyor
-echo dump_select            = 1                     >> wsock_trace.appveyor
-echo dump_hostent           = 1                     >> wsock_trace.appveyor
-echo dump_protoent          = 1                     >> wsock_trace.appveyor
-echo dump_servent           = 1                     >> wsock_trace.appveyor
-echo dump_nameinfo          = 1                     >> wsock_trace.appveyor
-echo dump_wsaprotocol_info  = 1                     >> wsock_trace.appveyor
-echo dump_wsanetwork_events = 1                     >> wsock_trace.appveyor
-echo dump_data              = 1                     >> wsock_trace.appveyor
-echo max_data               = 5000                  >> wsock_trace.appveyor
-echo max_displacement       = 1000                  >> wsock_trace.appveyor
-echo exclude                = htons,htonl,inet_addr >> wsock_trace.appveyor
-echo hosts_file             = %CD%\hosts            >> wsock_trace.appveyor
-echo [geoip]                                        >> wsock_trace.appveyor
-echo enable                 = 1                     >> wsock_trace.appveyor
-echo use_generated          = 0                     >> wsock_trace.appveyor
-echo max_days               = 10                    >> wsock_trace.appveyor
-echo geoip4_file            = %CD%\geoip            >> wsock_trace.appveyor
-echo geoip6_file            = %CD%\geoip6           >> wsock_trace.appveyor
-echo ip2location_bin_file   = %CD%\IP46-COUNTRY.BIN >> wsock_trace.appveyor
-echo [idna]                                         >> wsock_trace.appveyor
-echo enable                 = 1                     >> wsock_trace.appveyor
-echo [lua]                                          >> wsock_trace.appveyor
-echo enable      = %%WSOCK_LUA_ENABLE%%             >> wsock_trace.appveyor
-echo trace_level = 1                                >> wsock_trace.appveyor
-echo lua_init    = %CD%\src\wsock_trace_init.lua    >> wsock_trace.appveyor
-echo lua_exit    = %CD%\src\wsock_trace_exit.lua    >> wsock_trace.appveyor
-echo [dnsbl]                                        >> wsock_trace.appveyor
-echo enable      = 1                                >> wsock_trace.appveyor
-echo drop_file   = %CD%\drop.txt                    >> wsock_trace.appveyor
-echo edrop_file  = %CD%\edrop.txt                   >> wsock_trace.appveyor
-echo dropv6_file = %CD%\dropv6.txt                  >> wsock_trace.appveyor
+echo #                                                       > wsock_trace.appveyor
+echo # This file was generated from %0.                     >> wsock_trace.appveyor
+echo #                                                      >> wsock_trace.appveyor
+echo [core]                                                 >> wsock_trace.appveyor
+echo trace_level            = %%WSOCK_TRACE_LEVEL%%         >> wsock_trace.appveyor
+echo trace_indent           = 2                             >> wsock_trace.appveyor
+echo trace_caller           = 1                             >> wsock_trace.appveyor
+echo trace_report           = %%WSOCK_TRACE_LEVEL%%         >> wsock_trace.appveyor
+echo trace_time             = relative                      >> wsock_trace.appveyor
+echo trace_max_len          = %%COLUMNS%%                   >> wsock_trace.appveyor
+echo callee_level           = 1                             >> wsock_trace.appveyor
+echo cpp_demangle           = 1                             >> wsock_trace.appveyor
+echo short_errors           = 1                             >> wsock_trace.appveyor
+echo use_full_path          = 1                             >> wsock_trace.appveyor
+echo use_toolhlp32          = 1                             >> wsock_trace.appveyor
+echo dump_modules           = 0                             >> wsock_trace.appveyor
+echo dump_select            = 1                             >> wsock_trace.appveyor
+echo dump_hostent           = 1                             >> wsock_trace.appveyor
+echo dump_protoent          = 1                             >> wsock_trace.appveyor
+echo dump_servent           = 1                             >> wsock_trace.appveyor
+echo dump_nameinfo          = 1                             >> wsock_trace.appveyor
+echo dump_wsaprotocol_info  = 1                             >> wsock_trace.appveyor
+echo dump_wsanetwork_events = 1                             >> wsock_trace.appveyor
+echo dump_data              = 1                             >> wsock_trace.appveyor
+echo max_data               = 5000                          >> wsock_trace.appveyor
+echo max_displacement       = 1000                          >> wsock_trace.appveyor
+echo exclude                = htons,htonl,inet_addr         >> wsock_trace.appveyor
+echo hosts_file             = %CD%\hosts                    >> wsock_trace.appveyor
+echo [geoip]                                                >> wsock_trace.appveyor
+echo enable                 = 1                             >> wsock_trace.appveyor
+echo use_generated          = 0                             >> wsock_trace.appveyor
+echo max_days               = 10                            >> wsock_trace.appveyor
+echo geoip4_file            = %CD%\geoip                    >> wsock_trace.appveyor
+echo geoip6_file            = %CD%\geoip6                   >> wsock_trace.appveyor
+echo ip2location_bin_file   = %CD%\IP46-COUNTRY.BIN         >> wsock_trace.appveyor
+echo [idna]                                                 >> wsock_trace.appveyor
+echo enable                 = 1                             >> wsock_trace.appveyor
+echo [lua]                                                  >> wsock_trace.appveyor
+echo enable      = %%WSOCK_LUA_ENABLE%%                     >> wsock_trace.appveyor
+echo trace_level = 1                                        >> wsock_trace.appveyor
+echo lua_init    = %CD%\src\wsock_trace_init.lua            >> wsock_trace.appveyor
+echo lua_exit    = %CD%\src\wsock_trace_exit.lua            >> wsock_trace.appveyor
+echo [dnsbl]                                                >> wsock_trace.appveyor
+echo enable      = 1                                        >> wsock_trace.appveyor
+echo max_days    = 0                                        >> wsock_trace.appveyor
+echo drop_file   = %CD%\drop.txt                            >> wsock_trace.appveyor
+echo edrop_file  = %CD%\edrop.txt                           >> wsock_trace.appveyor
+echo dropv6_file = %CD%\dropv6.txt                          >> wsock_trace.appveyor
+echo drop_url    = http://www.spamhaus.org/drop/drop.txt    >> wsock_trace.appveyor
+echo edrop_url   = http://www.spamhaus.org/drop/edrop.txt   >> wsock_trace.appveyor
+echo dropv6_url  = https://www.spamhaus.org/drop/dropv6.txt >> wsock_trace.appveyor
 
 echo Generating hosts file...
 echo #                                               > hosts
