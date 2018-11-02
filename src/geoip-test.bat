72e8d0c5f6d16eb40296ff18c475b93ab9c27553
Gisle Vanem <gisle.vanem@gmail.com>
2018-11-02 23:03:57 +0000

    Rewritten without using '..\IP2Location-C-Library\test\country_test_ipv*_data.txt' which are now history.

--- src/geoip-test.bat
+++ src/geoip-test.bat
@@ -5,13 +5,15 @@
 ::
 setlocal
 set WSOCK_TRACE_LEVEL=0
+set TEST_INPUT_4=%TEMP%\geoip-addr_4.test
+set TEST_INPUT_6=%TEMP%\geoip-addr_6.test
 
 if %1. == -h. (
   echo Usage: %0 [-h ^| -d ^| --ip2loc_4 ^| --ip2loc_6]
   echo ^    -h:         this help.
   echo ^    -d:         sets "WSOCK_TRACE_LEVEL=2".
-  echo ^    --ip2loc_4: test using addresses in "..\IP2Location-C-Library\test\country_test_ipv4_data.txt".
-  echo ^    --ip2loc_6: test using addresses in "..\IP2Location-C-Library\test\country_test_ipv6_data.txt".
+  echo ^    --ip2loc_4: test using addresses in "%TEST_INPUT_4%".
+  echo ^    --ip2loc_6: test using addresses in "%TEST_INPUT_6%".
   exit /b 0
 )
 
@@ -20,65 +22,84 @@ if %1. == -d. (
   shift
 )
 
-set TEST_INPUT=%TEMP%\geoip-addr.test
-echo Generating %TEST_INPUT%...
+::
+:: Previously data from:
+::  ..\IP2Location-C-Library\test\country_test_ipv4_data.txt
+:: + some more
+::
+echo Generating %TEST_INPUT_4%...
+
+echo 19.5.10.1         > %TEST_INPUT_4%
+echo 25.5.10.2        >> %TEST_INPUT_4%
+echo 43.5.10.3        >> %TEST_INPUT_4%
+echo 47.5.10.4        >> %TEST_INPUT_4%
+echo 51.5.10.5        >> %TEST_INPUT_4%
+echo 53.5.10.6        >> %TEST_INPUT_4%
+echo 80.5.10.7        >> %TEST_INPUT_4%
+echo 81.5.10.8        >> %TEST_INPUT_4%
+echo 83.5.10.9        >> %TEST_INPUT_4%
+echo 85.5.10.0        >> %TEST_INPUT_4%
+echo 194.38.123.15    >> %TEST_INPUT_4%
+echo 218.156.62.27    >> %TEST_INPUT_4%
+echo 104.218.222.242  >> %TEST_INPUT_4%
+echo 69.86.117.107    >> %TEST_INPUT_4%
+echo 36.237.237.167   >> %TEST_INPUT_4%
+echo 99.163.121.226   >> %TEST_INPUT_4%
+echo 101.159.46.181   >> %TEST_INPUT_4%
+echo 78.33.206.219    >> %TEST_INPUT_4%
+echo 96.135.76.208    >> %TEST_INPUT_4%
+echo 192.31.5.47      >> %TEST_INPUT_4%
+echo 244.210.76.66    >> %TEST_INPUT_4%
+echo 210.113.60.169   >> %TEST_INPUT_4%
+echo 59.169.95.118    >> %TEST_INPUT_4%
+echo 86.104.62.176    >> %TEST_INPUT_4%
+echo 170.185.77.168   >> %TEST_INPUT_4%
+echo 201.227.72.250   >> %TEST_INPUT_4%
+echo 226.212.139.179  >> %TEST_INPUT_4%
+echo 247.140.100.112  >> %TEST_INPUT_4%
+::
+:: Add some IPv4 addresses from SpamHaus' DROP.txt too:
+::
+echo 23.226.48.10  # part of 23.226.48.0/20  ; SBL322605 >> %TEST_INPUT_4%
+echo 84.238.160.4  # part of 84.238.160.0/22 ; SBL339089 >> %TEST_INPUT_4%
+
+::
+:: Previously data from:
+::  ..\IP2Location-C-Library\test\country_test_ipv6_data.txt
+:: + some more.
+::
+echo Generating %TEST_INPUT_6%...
+
+echo 2001:0200:0102::      JP  > %TEST_INPUT_6%
+echo 2a01:04f8:0d16:25c2:: DE >> %TEST_INPUT_6%
+echo 2a01:04f8:0d16:26c2:: DE >> %TEST_INPUT_6%
+echo 2a01:ad20::           ES >> %TEST_INPUT_6%
+echo 2a01:af60::           PL >> %TEST_INPUT_6%
+echo 2a01:b200::           SK >> %TEST_INPUT_6%
+echo 2a01:b340::           IE >> %TEST_INPUT_6%
+echo 2a01:b4c0::           CZ >> %TEST_INPUT_6%
+echo 2a01:b600:8001::      IT >> %TEST_INPUT_6%
+echo 2a01:b6c0::           SE >> %TEST_INPUT_6%
+::
+:: Add some IPv6 addresses from SpamHaus' DROPv6.txt too:
+::
+echo 2a06:f680::3   # part of 2a06:f680::/29 ; SBL303641 >> %TEST_INPUT_6%
+echo 2a07:9b80::33  # part of 2a07:9b80::/29 ; SBL342980 >> %TEST_INPUT_6%
 
 if %1. == --ip2loc_4. (
-  grep.exe --only-matching "[0-9\.]*" ..\IP2Location-C-Library\test\country_test_ipv4_data.txt > %TEST_INPUT%
   rem
   rem Add some IPv4 addresses from SpamHaus' DROP.txt too:
   rem
-  echo 36.37.48.34     # part of 36.37.48.0/20   ; SBL258006 >> %TEST_INPUT%
-  echo 121.46.124.33   # part of 121.46.124.0/22 ; SBL287428 >> %TEST_INPUT%
-  %~dp0geoip.exe -4 %2 %3 %4 @%TEST_INPUT%
+  echo 36.37.48.34     # part of 36.37.48.0/20   ; SBL258006 >> %TEST_INPUT_4%
+  echo 121.46.124.33   # part of 121.46.124.0/22 ; SBL287428 >> %TEST_INPUT_4%
+  %~dp0geoip.exe -4 %2 %3 %4 @%TEST_INPUT_4%
   exit /b 0
 )
 
 if %1. == --ip2loc_6. (
-  grep.exe --only-matching "[0-9a-f\:]*" ..\IP2Location-C-Library\test\country_test_ipv6_data.txt > %TEST_INPUT%
-  rem
-  rem Add some IPv6 addresses from SpamHaus' DROPv6.txt too:
-  rem
-  echo 2a06:f680::3    # part of 2a06:f680::/29 ; SBL303641 >> %TEST_INPUT%
-  echo 2a07:9b80::33   # part of 2a07:9b80::/29 ; SBL342980 >> %TEST_INPUT%
-  %~dp0geoip.exe -6 %2 %3 %4 @%TEST_INPUT%
+  %~dp0geoip.exe -6 %2 %3 %4 @%TEST_INPUT_6%
   exit /b 0
 )
 
-echo 19.5.10.1      # US  > %TEST_INPUT%
-echo 25.5.10.2      # GB >> %TEST_INPUT%
-echo 43.5.10.3      # JP >> %TEST_INPUT%
-echo 47.5.10.4      # CA >> %TEST_INPUT%
-echo 51.5.10.5      # DE >> %TEST_INPUT%
-echo 53.5.10.6      # DE >> %TEST_INPUT%
-echo 80.5.10.7      # GB >> %TEST_INPUT%
-echo 81.5.10.8      # IL >> %TEST_INPUT%
-echo 83.5.10.9      # PL >> %TEST_INPUT%
-echo 85.5.10.0      # CH >> %TEST_INPUT%
-echo 194.38.123.15       >> %TEST_INPUT%
-echo 218.156.62.27       >> %TEST_INPUT%
-echo 104.218.222.242     >> %TEST_INPUT%
-echo 69.86.117.107       >> %TEST_INPUT%
-echo 36.237.237.167      >> %TEST_INPUT%
-echo 99.163.121.226      >> %TEST_INPUT%
-echo 101.159.46.181      >> %TEST_INPUT%
-echo 78.33.206.219       >> %TEST_INPUT%
-echo 96.135.76.208       >> %TEST_INPUT%
-echo 192.31.5.47         >> %TEST_INPUT%
-echo 244.210.76.66       >> %TEST_INPUT%
-echo 210.113.60.169      >> %TEST_INPUT%
-echo 59.169.95.118       >> %TEST_INPUT%
-echo 86.104.62.176       >> %TEST_INPUT%
-echo 170.185.77.168      >> %TEST_INPUT%
-echo 201.227.72.250      >> %TEST_INPUT%
-echo 226.212.139.179     >> %TEST_INPUT%
-echo 247.140.100.112     >> %TEST_INPUT%
+type %TEST_INPUT_4% | %~dp0geoip.exe -4 %1 %2 %3
 
-::
-:: Add some IPv4 addresses from SpamHaus' DROP.txt too:
-::
-echo 23.226.48.10  # part of 23.226.48.0/20  ; SBL322605 >> %TEST_INPUT%
-echo 84.238.160.4  # part of 84.238.160.0/22 ; SBL339089 >> %TEST_INPUT%
-
-@echo on
-type %TEST_INPUT% | %~dp0geoip.exe -4 %1 %2 %3
