ef62df1752a3596f9f942dc9a3230ef00cf69930
Gisle Vanem <gisle.vanem@gmail.com>
2018-11-15 12:11:35 +0000

    firewall.c is a NOP for Watcom.

--- src/firewall.h
+++ src/firewall.h
@@ -4,11 +4,23 @@
 #ifndef _FIREWALL_H
 #define _FIREWALL_H
 
-extern BOOL        fw_init (void);
-extern void        fw_exit (void);
-extern BOOL        fw_monitor_start (void);
-extern void        fw_monitor_stop (void);
-extern const char *fw_strerror (DWORD err);
+#if defined(__WATCOMC__)
+  /*
+   * OpenWatcom 2.x is hardly able to compile and use anything in firewall.c.
+   */
+  #define fw_init()           FALSE
+  #define fw_exit()           (void)0
+  #define fw_monitor_start()  FALSE
+  #define fw_monitor_stop()   (void)0
+  #define fw_strerror(err)    NULL
+
+#else
+  extern BOOL        fw_init (void);
+  extern void        fw_exit (void);
+  extern BOOL        fw_monitor_start (void);
+  extern void        fw_monitor_stop (void);
+  extern const char *fw_strerror (DWORD err);
+#endif
 
 #endif /* _FIREWALL_H */
 
