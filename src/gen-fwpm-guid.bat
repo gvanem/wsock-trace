344e856480aa42cd22b2d3ce9bc360df2ffe8753
Gisle Vanem <gisle.vanem@gmail.com>
2020-03-07 10:47:57 +0000

    Update for the 'cmd' junk-shell.

--- src/gen-fwpm-guid.bat
+++ src/gen-fwpm-guid.bat
@@ -12,11 +12,17 @@
 ::
 @echo off
 setlocal
+:: if %_cmdproc. neq 4NT. (set prompt=$P$G & echo on)
+
 set CFLAGS=-nologo -E -D_WIN32_WINNT=0xA000
 set GREP_OPT=--before-context=1 --after-context=5
+set GUID_C=%TEMP%\gen-fwpm-guid.c
+set GUID_TMP=%TEMP%\gen-fwpm-guid.tmp
+set GUID_RES=%TEMP%\gen-fwpm-guid.result
 
-echo `#include <fwpmu.h>`          > %TEMP%\gen-fwpm-guid.c
-cl %CFLAGS% %TEMP%\gen-fwpm-guid.c > %TEMP%\gen-fwpm-guid.tmp
+echo #include "fwpmu.h"  > %GUID_C%
+cl.exe %CFLAGS% %GUID_C% > %GUID_TMP%
 
-grep %GREP_OPT% '   FWPM_LAYER_.*,' %TEMP%\gen-fwpm-guid.tmp | ^
-     sed -e 's/--//' -e 's/DEFINE_GUID(/_DEFINE_GUID (/'
+grep.exe %GREP_OPT% "   FWPM_LAYER_.*," %GUID_TMP% | sed.exe -e "s@--@@" -e "s@DEFINE_GUID(@_DEFINE_GUID (@" > %GUID_RES%
+
+echo Done. Look in %GUID_RES%
