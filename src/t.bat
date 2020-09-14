f08b84268c851e790a828340be98bc7e453d67c6
Gisle Vanem <gisle.vanem@gmail.com>
2020-09-14 20:24:55 +0000

    Removed more unrelated files.

--- src/t.bat
+++
@@ -1,5 +0,0 @@
-git add  ../.gitattributes  ../.gitignore ../README.md ../appveyor-script.bat ../appveyor.yml ../appveyor_hosts_content.txt
-
-git add ../doc/Makefile ../doc/wsock_trace.css ../doc/main_page.dox ../doc/wsock_trace.Doxyfile ../doc/README.md
-
-
--- src/wmi-ver.vbs
+++
@@ -1,8 +0,0 @@
-' Run as:
-'   cscript -nologo wmi-ver.vbs
-
-set WMI = GetObject ("WinMgmts:{impersonationLevel=impersonate}!/root/cimv2")
-set obj = WMI.Get ("Win32_WMISetting=@")
-
-WScript.Echo obj.BuildVersion
-
