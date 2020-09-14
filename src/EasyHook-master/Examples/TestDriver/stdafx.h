b589d02de21d6c84ee5e434c3e4937ff647f7602
Gisle Vanem <gisle.vanem@gmail.com>
2020-09-14 20:17:41 +0000

    Removed some unrelated files.

--- src/EasyHook-master/Examples/TestDriver/stdafx.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STDAFX_H_
-#define _STDAFX_H_
-
-#include "easyhook.h"
-#include "drivershared.h"
-
-EASYHOOK_NT_INTERNAL RunTestSuite();
-
-#endif
--- src/EasyHook-master/tools/EasyHookNativePkg.ps1
+++
@@ -1,1 +0,0 @@
-Write-NuGetPackage .\EasyHookNative.autopkg
