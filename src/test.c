cf5e3a78bb5f041445becb89dbc3ad946d92eef5
Gisle Vanem <gisle.vanem@gmail.com>
2020-10-10 14:05:13 +0200

    Aded test functions:   'test_inet_ntop()' and 'test_inet_pton()'. Added some illegal test-addresses. Like:   'InetPtonW (AF_INET6, L"2H00:1450:400F:805::GGGG", &in6)' Rewritten to print in colours:   "OKAY" printed in green and "FAILED" in red.   Uses SGR-sequences (ANSI) under CygWin or AppVeyor.

--- src/test.c
+++ src/test.c
@@ -36,6 +36,10 @@
   #define USE_WSAPoll 0
 #endif
 
+#ifndef AF_UNIX
+#define AF_UNIX 1
+#endif
+
 GCC_PRAGMA (GCC diagnostic ignored "-Wstrict-aliasing")
 GCC_PRAGMA (GCC diagnostic ignored "-Wpointer-to-int-cast")
 
@@ -102,6 +106,8 @@ const char *program_name = "test.exe";
 static void test_condition (int okay, const char *function);
 static void test_string (const char *expect, const char *result, const char *function);
 static void test_wstring (const wchar_t *expect, const wchar_t *result, const char *function);
+static void test_okay (const char *fmt, ...);
+static void test_failed (const char *fmt, ...);
 
 static void test_ptr_or_error32 (void);
 static void test_ptr_or_error32b (void);
@@ -121,10 +127,14 @@ static void test_socket (void);
 static void test_socket_unix (void);
 static void test_ioctlsocket (void);
 static void test_connect (void);
-static void test_select (void);
+static void test_select1 (void);
 static void test_select2 (void);
        int  test_select3 (void);
 static void test_send (void);
+static void test_inet_ntop (void);
+static void test_inet_pton (void);
+static void test_InetPtonW (void);
+static void test_InetNtopW (void);
 static void test_WSAPoll (void);
 static void test_WSAFDIsSet (void);
 static void test_WSAAddressToStringA (void);
@@ -133,8 +143,6 @@ static void test_WSAAddressToStringWP (void);
 static void test_WSAStringToAddressA (void);
 static void test_WSAStringToAddressW (void);
 static void test_WSAEnumProtocols (void);
-static void test_InetPtonW (void);
-static void test_InetNtopW (void);
 static void test_IDNA_functions (void);
 
 /*
@@ -167,9 +175,13 @@ static const struct test_struct tests[] = {
                     ADD_TEST (socket_unix),
                     ADD_TEST (ioctlsocket),
                     ADD_TEST (connect),
-                    ADD_TEST (select),
+                    ADD_TEST (select1),
                     ADD_TEST (select2),
                     ADD_TEST (send),
+                    ADD_TEST (inet_ntop),
+                    ADD_TEST (inet_pton),
+                    ADD_TEST (InetPtonW),
+                    ADD_TEST (InetNtopW),
                     ADD_TEST (WSAPoll),
                     ADD_TEST (WSAFDIsSet),
                     ADD_TEST (WSAAddressToStringA),
@@ -178,8 +190,6 @@ static const struct test_struct tests[] = {
                     ADD_TEST (WSAStringToAddressA),
                     ADD_TEST (WSAStringToAddressW),
                     ADD_TEST (WSAEnumProtocols),
-                    ADD_TEST (InetPtonW),
-                    ADD_TEST (InetNtopW),
                     ADD_TEST (WSACleanup)
                   };
 
@@ -206,6 +216,7 @@ static int run_test (const char *wildcard)
     rc++;
     if (chatty >= 1)
        printf ("\nTesting %s():\n", t->name);
+    WSASetLastError (0);   /* clear any previous error */
     (*t->func)();
   }
   return (rc);
@@ -239,9 +250,11 @@ static void test_gethostbyaddr (void)
   ia = (const char*) &ia6;         /* '::' -> hostname of this machine */
   TEST_CONDITION (!= 0, gethostbyaddr (ia, sizeof(ia6), AF_INET6));
 
-  /* Some www.google.com IPv6 addresses: Should be in Ireland.
+  /* Some www.google.com IPv6 addresses: Should be in Ireland and
+   * reverse resolve to 'arn09s20-in-x04.1e100.net'.
+   * But that can change tomorrow.
    */
-  TEST_CONDITION (== 1, inet_pton (AF_INET6, "2A00:1450:400F:805::1011", &ia6.s6_addr));
+  TEST_CONDITION (== 1, inet_pton (AF_INET6, "2A00:1450:400f:80D::2004", &ia6.s6_addr));
   TEST_CONDITION (!= 0, gethostbyaddr (ia, sizeof(ia6), AF_INET6));
 
   /* Should be in Finland.
@@ -331,7 +344,6 @@ static void test_getnameinfo (void)
   flags = NI_DGRAM;
   TEST_CONDITION (== 0, getnameinfo (sa, sizeof(sa4), host, sizeof(host), NULL, 0, flags));
 
-#if !defined(__CYGWIN__) /* Doesn't like the '.s6_bytes' */
   sa6.sin6_family = AF_INET6;
   sa6.sin6_addr.s6_bytes[0] = 0x2C; /* Should return 'geo-IP: KE - Kenya'. */
   sa6.sin6_addr.s6_bytes[1] = 0x0F;
@@ -340,7 +352,6 @@ static void test_getnameinfo (void)
   sa6.sin6_port = htons (80);
   sa = (const struct sockaddr*) &sa6;
   TEST_CONDITION (== 0, getnameinfo (sa, sizeof(sa6), host, sizeof(host), NULL, 0, flags));
-#endif
 
   /* Should be Finland (Lappeenranta/Etela-Karjala)
    */
@@ -391,8 +402,8 @@ static void test_gai_strerror (void)
    * functions in <ws2tcpip.h>. So these should always pass (a test that
    * 'test_string()' + 'test_wstring()' works okay)
    */
-  TEST_STRING  (err_buf1, gai_strerrorA(rc));
-  TEST_WSTRING (err_buf2, gai_strerrorW(rc), "gai_strerrorW");
+  TEST_STRING  (err_buf1, gai_strerrorA (rc));
+  TEST_WSTRING (err_buf2, gai_strerrorW (rc), "gai_strerrorW (rc)");
 }
 
 static SOCKET s1, s2;
@@ -411,11 +422,12 @@ static void test_socket (void)
 static void test_socket_unix (void)
 {
   s1_unix = socket (AF_UNIX, SOCK_STREAM, 0);
-  s2_unix = socket (AF_UNIX, SOCK_DGRAM, 0);
-
   TEST_CONDITION (!= INVALID_SOCKET, s1_unix);
-  TEST_CONDITION (!= INVALID_SOCKET, s2_unix);
   TEST_CONDITION (== 0, WSAGetLastError());
+
+  s2_unix = socket (AF_UNIX, SOCK_DGRAM, 0);
+  TEST_CONDITION (== INVALID_SOCKET, s2_unix);    /* AF_UNIX on SOCK_DGRAM is unsupported */
+  TEST_CONDITION (== WSAEAFNOSUPPORT, WSAGetLastError());
 }
 
 static void test_ioctlsocket (void)
@@ -426,6 +438,19 @@ static void test_ioctlsocket (void)
   TEST_CONDITION (== 0, WSAGetLastError());
 }
 
+/*
+ * If this program was run like:
+ *   test.exe -d wsastartup socket connect
+ *
+ * the below WSAGetLastError() would return WSAECONNREFUSED.
+ *
+ * If this program was run like:
+ *   test.exe -d wsastartup socket ioctlsocket connect
+ *
+ * the below WSAGetLastError() would return WSAEWOULDBLOCK.
+ *
+ * Hence test for both error-codes.
+ */
 static void test_connect (void)
 {
   struct sockaddr_in sa4;
@@ -436,12 +461,12 @@ static void test_connect (void)
   sa4.sin_addr.s_addr = htonl (INADDR_LOOPBACK);
   sa4.sin_port = htons (1234);
   TEST_CONDITION (== -1, connect (s1, sa, sizeof(sa4)));
-  TEST_CONDITION (== WSAEWOULDBLOCK, WSAGetLastError());
+  TEST_CONDITION (== (WSAEWOULDBLOCK || WSAECONNREFUSED), WSAGetLastError());
 }
 
 static fd_set fd1, fd2;
 
-static void test_select (void)
+static void test_select1 (void)
 {
   struct timeval  tv = { 1, 1 };
   int    i;
@@ -500,7 +525,7 @@ int test_select3 (void)
   s = socket (AF_INET, SOCK_STREAM, 0);
   FD_SET (s, &fd);
 
-  TEST_CONDITION (== -1, select (0, &fd, &fd, NULL, &tv));
+  TEST_CONDITION (== 0, select (0, &fd, &fd, NULL, &tv));
   TEST_CONDITION (== 0, WSACleanup());
   return (0);
 }
@@ -522,14 +547,14 @@ static void test_WSAPoll (void)
   struct pollfd poll[2];
 
   poll[0].fd      = s1;
-  poll[0].events  = POLLOUT;
+  poll[0].events  = POLLOUT | POLLIN;
   poll[0].revents = 0;
 
   poll[1].fd      = s2;
-  poll[1].events  = POLLOUT;
+  poll[1].events  = POLLOUT | POLLIN;
   poll[1].revents = 0;
 
-  TEST_CONDITION (== 1, WSAPoll ((LPWSAPOLLFD)&poll, 2, 10));
+  TEST_CONDITION (== 1, WSAPoll ((WSAPOLLFD*)&poll, 2, 100));
 #else
   if (chatty >= 1)
      puts ("  disabled.");
@@ -649,6 +674,41 @@ static void test_WSAEnumProtocols (void)
   TEST_CONDITION ( > 0, WSAEnumProtocols (NULL, p_info, &len));
 }
 
+static void test_inet_pton (void)
+{
+  struct in6_addr in6;
+  struct in_addr  in4;
+
+  TEST_CONDITION (== 1, inet_pton (AF_INET, "127.0.0.1", &in4));
+  TEST_CONDITION (== 0, inet_pton (AF_INET, "a.b.c.d", &in4));
+  TEST_CONDITION (== 1, inet_pton (AF_INET6, "2A00:1450:400F:805::1011", &in6));
+  TEST_CONDITION (== 0, inet_pton (AF_INET6, "2H00:1450:400F:805::101H", &in6));
+}
+
+static void test_inet_ntop (void)
+{
+  struct in_addr  in4;
+  struct in6_addr in6;
+  char   buf4 [INET_ADDRSTRLEN];
+  char   buf6 [INET6_ADDRSTRLEN];
+
+  in4.s_addr = inet_addr ("127.0.0.1");
+  in6.s6_words[0] = htons (0x2A00);
+  in6.s6_words[1] = htons (0x1450);
+  in6.s6_words[2] = htons (0x400f);
+  in6.s6_words[3] = htons (0x0805);
+  in6.s6_words[4] = 0;
+  in6.s6_words[5] = 0;
+  in6.s6_words[6] = 0;
+  in6.s6_words[7] = htons (0x1011);
+
+  TEST_CONDITION (== (int)&buf4, inet_ntop (AF_INET, &in4, buf4, sizeof(buf4)));
+  TEST_CONDITION (== (int)&buf6, inet_ntop (AF_INET6, &in6, buf6, sizeof(buf6)));
+
+  in4.s_addr = inet_addr ("127.0.0.1");
+  TEST_CONDITION (== 0, inet_ntop (AF_UNIX, &in4, buf4, sizeof(buf4)));
+}
+
 static void test_InetPtonW (void)
 {
 #if !defined(__WATCOMC__)
@@ -656,7 +716,9 @@ static void test_InetPtonW (void)
   struct in6_addr in6;
 
   TEST_CONDITION (== 1, InetPtonW (AF_INET, L"127.0.0.1", &in4));
+  TEST_CONDITION (== 0, InetPtonW (AF_INET, L"a.b.c.d", &in4));
   TEST_CONDITION (== 1, InetPtonW (AF_INET6, L"2A00:1450:400F:805::1011", &in6));
+  TEST_CONDITION (== 0, InetPtonW (AF_INET6, L"2H00:1450:400F:805::GGGG", &in6));
 #endif
 }
 
@@ -668,8 +730,10 @@ static void test_InetNtopW (void)
   wchar_t buf [INET6_ADDRSTRLEN];
 
   in4.s_addr = inet_addr ("127.0.0.1");
+  TEST_CONDITION (== 0, InetNtopW (AF_INET, &in4, buf, 1));
   TEST_CONDITION (!= 0, InetNtopW (AF_INET, &in4, buf, sizeof(buf)/2));
   TEST_CONDITION (!= 0, InetNtopW (AF_INET6, &in6, buf, sizeof(buf)/2));
+  TEST_CONDITION (== 0, InetNtopW (AF_UNIX, &in4, buf, sizeof(buf)/2));
 #endif
 }
 
@@ -786,7 +850,7 @@ static int list_tests (void)
   puts ("List of tests:");
   for (i = 0; i < DIM(tests); i++, t++)
   {
-    assert (strncmp(t->name,"test_",pfx_len) == 0);
+    assert (strncmp(t->name, "test_", pfx_len) == 0);
     printf ("  %s()\n", t->name + pfx_len);
   }
   return (0);
@@ -925,17 +989,17 @@ static const char *ptr_or_error64 (ULONG64 x)
 
 static void test_ptr_or_error32 (void)
 {
-  TEST_STRING ("0x11223344", ptr_or_error32(0x11223344));
+  TEST_STRING ("0x11223344", ptr_or_error32 (0x11223344));
 }
 
 static void test_ptr_or_error32b (void)
 {
-  TEST_STRING ("0x11223345", ptr_or_error32(0x11223345));
+  TEST_STRING ("0x11223345", ptr_or_error32 (0x11223345));
 }
 
 static void test_ptr_or_error64 (void)
 {
-  TEST_STRING ("0x11223344AABBCCDD", ptr_or_error64(0x11223344AABBCCDD));
+  TEST_STRING ("0x11223344AABBCCDD", ptr_or_error64 (0x11223344AABBCCDD));
 }
 
 /**
@@ -946,10 +1010,9 @@ static void test_condition (int okay, const char *function)
 {
   if (chatty >= 1)
   {
-    printf ("  %-50s: ", function);
     if (okay)
-         puts ("OKAY");
-    else printf ("FAILED. result %d\n", last_result);
+         test_okay ("%s\n", function);
+    else test_failed ("%s: result %d.\n", function, last_result);
   }
 }
 
@@ -957,10 +1020,9 @@ static void test_string (const char *expect, const char *result, const char *fun
 {
   if (chatty >= 1)
   {
-    printf ("  %-50s: ", function);
     if (!strcmp(expect,result))
-         puts ("OKAY");
-    else printf ("FAILED. expected: '%s', got: '%s'\n", expect, result);
+         test_okay ("%s\n", function);
+    else test_failed ("%s: expected: '%s', got: '%s'\n", function, expect, result);
   }
 }
 
@@ -968,11 +1030,86 @@ static void test_wstring (const wchar_t *expect, const wchar_t *result, const ch
 {
   if (chatty >= 1)
   {
-    printf ("  %-50s: ", function);
-    if (!wcscmp(expect,result))
-         puts ("OKAY");
-    else printf ("FAILED. expected: '%S', got: '%S'\n", expect, result);
+    if (!wcscmp(expect, result))
+         test_okay ("%s\n", function);
+    else test_failed ("%s: expected: '%S', got: '%S'\n", function, expect, result);
   }
+}
+
+static CONSOLE_SCREEN_BUFFER_INFO c_info;
+static HANDLE c_hnd = INVALID_HANDLE_VALUE;
+
+#define COLOUR_GREEN  (FOREGROUND_INTENSITY | 2)
+#define COLOUR_RED    (FOREGROUND_INTENSITY | 4)
+
+#define SGR_GREEN     "\x1B[1;32m"
+#define SGR_RED       "\x1B[1;31m"
+#define SGR_DEFAULT   "\x1B[0m"
+
+static int use_wincon = -1;
+static int use_SGR = -1;     /* Use 'Select Graphic Rendition' codes under CygWin and AppVeyor */
+
+static void set_colour (int col)
+{
+  if (use_SGR == -1)
+  {
+    use_SGR = 0;
+#ifdef __CYGWIN__
+    use_SGR = 1;
+#else
+    if (getenv("APPVEYOR_BUILD_FOLDER"))
+       use_SGR = 1;
+#endif
+  }
+  if (use_wincon == -1)
+  {
+    c_hnd = GetStdHandle (STD_OUTPUT_HANDLE);
+    if (c_hnd != INVALID_HANDLE_VALUE &&
+        GetConsoleScreenBufferInfo(c_hnd, &c_info))
+         use_wincon = 1;
+    else use_wincon = 0;
+  }
+
+  if (use_SGR == 1)
+  {
+    if (col == 0)
+       fputs (SGR_DEFAULT, stdout);
+    else if (col == COLOUR_GREEN)
+       fputs (SGR_GREEN, stdout);
+    else if (col == COLOUR_RED)
+       fputs (SGR_RED, stdout);
+  }
+  else if (use_wincon == 1)
+  {
+    fflush (stdout);
+    if (col == 0)
+         SetConsoleTextAttribute (c_hnd, c_info.wAttributes);
+    else SetConsoleTextAttribute (c_hnd, (c_info.wAttributes & ~7) + col);
+  }
+}
+
+static void test_okay (const char *fmt, ...)
+{
+  va_list args;
+
+  set_colour (COLOUR_GREEN);
+  fputs ("  OKAY:   ", stdout);
+  set_colour (0);
+  va_start (args, fmt);
+  vprintf (fmt, args);
+  va_end (args);
+}
+
+static void test_failed (const char *fmt, ...)
+{
+  va_list args;
+
+  set_colour (COLOUR_RED);
+  fputs ("  FAILED: ", stdout);
+  set_colour (0);
+  va_start (args, fmt);
+  vprintf (fmt, args);
+  va_end (args);
 }
 
 static int name_match (const char *wildcard, const char *string)
