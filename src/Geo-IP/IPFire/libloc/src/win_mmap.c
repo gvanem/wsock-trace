9104befdb2adc80b40b2db6921105514268e6237
Gisle Vanem <gisle.vanem@gmail.com>
2020-10-21 09:08:55 +0200

    Add an array of 10 'mmap()' return-values to remember in 'mmap_storage[]'. Use this to lookup what to call 'UnmapViewOfFile()' with.

--- src/Geo-IP/IPFire/libloc/src/win_mmap.c
+++ src/Geo-IP/IPFire/libloc/src/win_mmap.c
@@ -7,15 +7,19 @@
 
 #define DWORD_HI(x)  ((uint64_t)(x) >> 32)
 #define DWORD_LO(x)  ((x) & 0xffffffff)
+#define DIM(arr)     (sizeof(arr) / sizeof(arr[0]))
 
 static int debug = -1;
 
-static void hex_dump (const char *what, const void *data_p, size_t datalen);
+static void *mmap_remember (void *map, off_t offset);
+static void *mmap_forget (void *map);
+static void  hex_dump (const char *what, const void *data_p, size_t datalen);
 
 void *_mmap (void *address, size_t length, int protection, int flags, int fd, off_t offset,
              const char *fname, unsigned line)
 {
   void        *map = NULL;
+  void        *rval = NULL;
   HANDLE       handle = INVALID_HANDLE_VALUE;
   DWORD        err1 = 0;
   DWORD        err2 = 0;
@@ -24,7 +28,7 @@ void *_mmap (void *address, size_t length, int protection, int flags, int fd, of
   uint64_t     pstart, psize, poffset;
   static DWORD block_size = 0;
 
-  (void) flags; /* Not used */
+  (void) flags;   /* Not used */
 
   if (block_size == 0)
   {
@@ -43,7 +47,7 @@ void *_mmap (void *address, size_t length, int protection, int flags, int fd, of
   switch (protection)
   {
     case PROT_READ:
-         handle = CreateFileMapping ((HANDLE)h, 0, PAGE_READONLY, 0, DWORD_LO(pstart), NULL);
+         handle = CreateFileMapping ((HANDLE)h, 0, PAGE_READONLY, 0, 0, NULL);
          access = FILE_MAP_READ;
          break;
     case PROT_WRITE:
@@ -76,22 +80,24 @@ void *_mmap (void *address, size_t length, int protection, int flags, int fd, of
   if (handle && handle != INVALID_HANDLE_VALUE)
      CloseHandle (handle);
 
+  rval = mmap_remember (map, offset);
+
   if (!debug)
-     return (map);
+     return (rval);
 
   if (map == MAP_FAILED)
   {
     fprintf (stderr, "%s(%u): pstart: %lld, poffset: %lld, psize: %lld\n", fname, line, pstart, poffset, psize);
     fprintf (stderr, "    address: 0x%p, length: %u, %d, err1: %lu, err2: %lu  -> 0x%p\n",
-             address, length, fd, err1, err2, map);
+             address, length, fd, err1, err2, rval);
   }
   else
   {
-    char  *p = (char*) map;
+    char  *p = (char*) rval;
     size_t len;
 
     fprintf (stderr, "%s(%u): address: 0x%p, length: %u, fd: %d, offset: %zu, err1: %lu, err2: %lu  -> 0x%p\n",
-             fname, line, address, length, fd, offset, err1, err2, map);
+             fname, line, address, length, fd, offset, err1, err2, rval);
 
     /* Now test the low and high end of the mmap'ed region to check
      * we get no exceptions. Check 'PROT_READ' only since that's the only
@@ -99,10 +105,6 @@ void *_mmap (void *address, size_t length, int protection, int flags, int fd, of
      */
     if (protection == PROT_READ)
     {
-      (void) p[0];
-      (void) p[length-1];
-      (void) p[length];
-
       len = min (length, 100);
       hex_dump ("Dumping first %zu bytes:\n", p, len);
       if (length > 100)
@@ -111,18 +113,62 @@ void *_mmap (void *address, size_t length, int protection, int flags, int fd, of
         fprintf (stderr, "Last chunk of data covered by the first chunk.\n\n");
     }
   }
-
-  return (map);
+  return (rval);
 }
 
 int _munmap (void *map, size_t length, const char *fname, unsigned line)
 {
+  map = mmap_forget (map);
+  if (map == MAP_FAILED)
+  {
+    fprintf (stderr, "%s(%u): munmap (0x%p, %zu) failed: %lu\n", fname, line, map, length, GetLastError());
+    errno = EINVAL;
+    return (-1);
+  }
   if (!UnmapViewOfFile(map) && debug)
-     fprintf (stderr, "%s(%u): munmap (0x%p) failed: %lu\n", fname, line, map, GetLastError());
-  (void) length;
+     fprintf (stderr, "%s(%u): munmap (0x%p, %zu) failed: %lu\n", fname, line, map, length, GetLastError());
   return (0);
 }
 
+/**
+ * A small array of 'mmap()' return values we need to remember
+ * until we call `munmap()` on the pointer.
+ */
+struct mmap_info {
+       void *map;   /* the value we got from MapViewOfFile() */
+       void *rval;  /* the value we returned in mmap() */
+     };
+static struct mmap_info mmap_storage[10];
+
+static void *mmap_remember (void *map, off_t offset)
+{
+  if (map == MAP_FAILED)  /* never rememember this */
+     return (MAP_FAILED);
+
+  for (int i = 0; i < DIM(mmap_storage); i++)
+  {
+    if (!mmap_storage[i].map)
+    {
+      mmap_storage[i].map = map;
+      mmap_storage[i].rval = (char*)map + offset;
+      return (mmap_storage[i].rval);
+    }
+  }
+  return (MAP_FAILED); /* all buckets full */
+}
+
+static void *mmap_forget (void *map)
+{
+  for (int i = 0; i < DIM(mmap_storage); i++)
+  {
+    if (map == mmap_storage[i].rval)
+    {
+      mmap_storage[i].map = NULL;   /* reuse this */
+      return (mmap_storage[i].rval);
+    }
+  }
+  return (MAP_FAILED);  /* not found! */
+}
 
 /**
  * Do not use 'hexdump()' in 'loc/private.h'.
