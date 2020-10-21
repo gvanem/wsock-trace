3ba9091c42f0fa2bf107e719746f8efc7e8d5be2
Gisle Vanem <gisle.vanem@gmail.com>
2020-10-21 12:45:18 +0200

    Restore to normal 'LOG_x' values.

--- src/Geo-IP/IPFire/libloc/src/loc/windows/syslog.h
+++ src/Geo-IP/IPFire/libloc/src/loc/windows/syslog.h
@@ -3,10 +3,14 @@
 
 /*
  * Minimal syslog.h for libloc on Windows.
- * Only the below 'LOG_x' vaues are needed.
  */
-#define LOG_ERR    1   /* error conditions */
-#define LOG_INFO   2   /* informational */
-#define LOG_DEBUG  3   /* debug-level messages */
+#define LOG_EMERG   0   /* system is unusable */
+#define LOG_ALERT   1   /* action must be taken immediately */
+#define LOG_CRIT    2   /* critical conditions */
+#define LOG_ERR     3   /* error conditions */
+#define LOG_WARNING 4   /* warning conditions */
+#define LOG_NOTICE  5   /* normal but significant condition */
+#define LOG_INFO    6   /* informational */
+#define LOG_DEBUG   7   /* debug-level messages */
 
 #endif
