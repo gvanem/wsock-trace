0d2ef20464746db4bab634fc4f3307a4ba534799
Gisle Vanem <gisle.vanem@gmail.com>
2020-12-29 12:22:12 +0100

    Get out early if called from 'DllMain()'. Since it is not safe to call any WinHTTP.dll function from inside a DLL.

--- src/inet_util.c
+++ src/inet_util.c
@@ -828,6 +828,12 @@ DWORD INET_util_download_file (const char *file, const char *url)
   BOOL              use_threaded = FALSE;
   BOOL              use_async    = FALSE;
 
+  if (g_cfg.from_dll_main)
+  {
+    TRACE (1, "Not safe to enter here from 'DllMain()'.\n");
+    return (0);
+  }
+
   if (g_cfg.use_winhttp)
   {
 #if defined(HAVE_WINHTTP_H)
