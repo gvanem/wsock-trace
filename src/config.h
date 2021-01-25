8c919d4153d242db13dcb5744fde9bd3eb8ca897
Gisle Vanem <gisle.vanem@gmail.com>
2021-01-25 16:43:44 +0100

    Added 'WIN32_LEAN_AND_MEAN' if not already.

--- src/config.h
+++ src/config.h
@@ -21,6 +21,12 @@
   #define HAVE_WINHTTP_H
 #endif
 
+/* Do not pull in <winsock.h> in <windows.h>
+ */
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 /* Because of warning "Use getaddrinfo() or GetAddrInfoW() instead ..." in idna.c.
  */
 #ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
