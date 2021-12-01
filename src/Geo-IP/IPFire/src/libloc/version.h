4323a6aa01720a5f7a2c53503cbb299f95e95125
Gisle Vanem <gvanem@yahoo.no>
2021-12-01 09:36:33 +0100

    Updated to latest IPFire/libloc version.

--- src/Geo-IP/IPFire/src/libloc/version.h
+++ src/Geo-IP/IPFire/src/libloc/version.h
@@ -1,11 +1,11 @@
   //
   // DO NOT EDIT! This file was automatically generated
-  // from F:/gv/VC_project/ws_trace/src/Geo-IP/IPFire/src/Makefile.Windows at 23-October-2021.
+  // from F:/gv/VC_project/ws_trace/src/Geo-IP/IPFire/src/Makefile.Windows at 01-December-2021.
   // Edit that file instead.
   //
  #ifndef LIBLOC_VERSION_H
  #define LIBLOC_VERSION_H
  #define LIBLOC_MAJOR_VER  0
  #define LIBLOC_MINOR_VER  9
- #define LIBLOC_MICRO_VER  8
+ #define LIBLOC_MICRO_VER  9
  #endif
