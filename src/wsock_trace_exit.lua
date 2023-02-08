2d9221f04482c3b2fd1b41510a7ef09cca92afd7
Gisle Vanem <gvanem@yahoo.no>
2023-02-08 04:50:29 +0100

    Fixed the code-style.

--- src/wsock_trace_exit.lua
+++ src/wsock_trace_exit.lua
@@ -4,16 +4,16 @@
 -- if called by lua / luajit directly
 --
 -- if (... ~= "wsock_trace_exit") then
---   dofile ("wsock_trace_init.lua")
+--    dofile ("wsock_trace_init.lua")
 -- end
 
 if ws.get_trace_level() >= 1 then
-  ws.C_puts (string.format("  Bye from ~1%s~0 at line %d\n", ws.__FILE__(), ws.__LINE__()))
+   ws.C_puts (string.format("  Bye from ~1%s~0 at line %d\n", ws.__FILE__(), ws.__LINE__()))
 end
 
 if package.loaded ["jit.profile"] then
-  ws.C_puts ("Stopping profiler\n")
-  profile.stop()
+   ws.C_puts ("Stopping profiler\n")
+   profile.stop()
 end
 
 return 3
--- src/wsock_trace_init.lua
+++ src/wsock_trace_init.lua
@@ -9,11 +9,11 @@ jit = require ("jit")
 ws_name = "wsock_trace"
 
 if package.loaded [ws_name] then
-  ws = require (ws_name)
-  print (string.format("  Package %s already loaded; ws -> %p", ws_name, ws))
+   ws = require (ws_name)
+   print (string.format("  Package %s already loaded; ws -> %p", ws_name, ws))
 else
-  print ("  Loading package 'wsock_trace'...");
-  ws = require (ws_name)
+   print ("  Loading package 'wsock_trace'...");
+   ws = require (ws_name)
 end
 
 
@@ -36,16 +36,16 @@ end
 
 function ws.local_trace (str)
   if ws.get_trace_level() >= 1 then
-    ws.C_puts (str)
+     ws.C_puts (str)
   else
-    io.write (str)
+     io.write (str)
   end
 end
 
 function dump_obj (name, obj)
   print (string.format("Type of '%s' is a %s:", name, type(obj)))
   for key, val in pairs(obj) do
-    print (string.format("  key: '%s', val: %s", key, val))
+      print (string.format("  key: '%s', val: %s", key, val))
   end
   print ("")
 end
@@ -62,10 +62,10 @@ print (string.format("  package.path:  %s", package.path))
 print (string.format("  package.cpath: %s", package.cpath))
 
 if ws.get_trace_level() >= 2 then
-  dump_obj ("jit.opt", jit.opt)
-  dump_obj ("jit.util", jit.util)
-  dump_obj ("ws", ws)
-  dump_obj ("_G", _G)
+   dump_obj ("jit.opt", jit.opt)
+   dump_obj ("jit.util", jit.util)
+   dump_obj ("ws", ws)
+   dump_obj ("_G", _G)
 end
 
 function C_printf (fmt, ...)
@@ -87,39 +87,39 @@ function profile_callback (th, samples, vmmode)
 end
 
 if nil and ws.get_profiler() then
-  profile = require ("jit.profile")
-  profile.start ("m", profile_callback)
+   profile = require ("jit.profile")
+   profile.start ("m", profile_callback)
 end
 
 if ws.get_trace_level() >= 1 then
-  ws.C_puts (string.format("  ws.get_trace_level: ~1%d~0.\n", ws.get_trace_level()))
+   ws.C_puts (string.format("  ws.get_trace_level: ~1%d~0.\n", ws.get_trace_level()))
 
-  if nil then
-    ws.C_puts ("  get_trace_level:    ~1" .. tostring(get_trace_level()) .. " ~0.\n")
-    ws.C_puts ("  ws.set_trace_level(0).\n")
-    ws.set_trace_level (0)
-  end
+   if nil then
+      ws.C_puts ("  get_trace_level:    ~1" .. tostring(get_trace_level()) .. " ~0.\n")
+      ws.C_puts ("  ws.set_trace_level(0).\n")
+      ws.set_trace_level (0)
+   end
 
-  if nil then
-    C_printf ("Hello from: ~1%s~0.\nVersion:  ~1%s~0. Arg-1: %d. Another arg: %s\n",
-              who_am_I, ws.os_details(), 10, "hello")
-  end
+   if nil then
+      C_printf ("Hello from: ~1%s~0.\nVersion:  ~1%s~0. Arg-1: %d. Another arg: %s\n",
+                who_am_I, ws.os_details(), 10, "hello")
+   end
 
-  -- ws.C_printf ("testing ws.C_printf():\n" ..
-  --              "  arg1 = ~1%s~0.\n" ..
-  --              "  arg2 = ~1%s~0.\n", who_am_I, ws.os_details())
+   -- ws.C_printf ("testing ws.C_printf():\n" ..
+   --              "  arg1 = ~1%s~0.\n" ..
+   --              "  arg2 = ~1%s~0.\n", who_am_I, ws.os_details())
 
-  ws.C_puts (string.format("  Hello from:         ~1%s~0.\n", who_am_I))
-  ws.C_puts (string.format("  Version:            ~1%s~0.\n", ws.os_details()))
-  ws.C_puts (string.format("  This is line:       ~1%d~0.\n", ws.__LINE__()))
+   ws.C_puts (string.format("  Hello from:         ~1%s~0.\n", who_am_I))
+   ws.C_puts (string.format("  Version:            ~1%s~0.\n", ws.os_details()))
+   ws.C_puts (string.format("  This is line:       ~1%d~0.\n", ws.__LINE__()))
 
-  ws.C_puts ("  package.path[]:     ~1" .. package.path .. "~0.\n")
-  ws.C_puts ("  package.cpath[]:    ~1" .. package.cpath .. "~0.\n")
+   ws.C_puts ("  package.path[]:     ~1" .. package.path .. "~0.\n")
+   ws.C_puts ("  package.cpath[]:    ~1" .. package.cpath .. "~0.\n")
 
-  ws.C_puts (string.format("  I'm importing from: ~1%s~0.\n", ws.get_dll_full_name()))
-  ws.C_puts (string.format("  ws.get_builder():   ~1%s~0.\n", ws.get_builder()))
-  ws.C_puts (string.format("  ws.get_version():   ~1%s~0.\n", ws.get_version()))
-  ws.C_puts (string.format("  ws.get_copyright(): ~1%s~0.\n", ws.get_copyright()))
+   ws.C_puts (string.format("  I'm importing from: ~1%s~0.\n", ws.get_dll_full_name()))
+   ws.C_puts (string.format("  ws.get_builder():   ~1%s~0.\n", ws.get_builder()))
+   ws.C_puts (string.format("  ws.get_version():   ~1%s~0.\n", ws.get_version()))
+   ws.C_puts (string.format("  ws.get_copyright(): ~1%s~0.\n", ws.get_copyright()))
 end
 
 -- ws.register_hook (ws.WSAStartup2, "2")
