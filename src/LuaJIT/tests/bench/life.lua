0016a320c5b176d660de6f1bbda4a48e953a98b0
Gisle Vanem <gvanem@yahoo.no>
2024-10-18 14:03:28 +0200

    Fix the code-style.

--- src/LuaJIT/tests/bench/life.lua
+++ src/LuaJIT/tests/bench/life.lua
@@ -9,23 +9,21 @@ ALIVE   = "\27[1;32mO\27[0m"
 DEAD    = "\27[37m-\27[0m"
 MAX_GEN = 500
 
-function delay(msec)
+function delay (msec)
   local now     = os.clock()
   local timeout = now + (msec / 1000)
 
-  -- write("now:     ", now, "\n")
-  -- write("timeout: ", timeout, "\n")
   while (now < timeout) do
     now = os.clock()
   end
 end
 
-function ARRAY2D(w,h)
-  local t = {w=w,h=h}
-  for y=1,h do
-    t[y] = {}
-    for x=1,w do
-      t[y][x]=0
+function ARRAY2D (w, h)
+  local t = {w = w, h= h}
+  for y = 1, h do
+    t [y] = {}
+    for x = 1, w do
+        t [y][x] = 0
     end
   end
   return t
@@ -33,84 +31,99 @@ end
 
 _CELLS = {}
 
+--
 -- give birth to a "shape" within the cell array
-function _CELLS:spawn(shape,left,top)
-  for y=0,shape.h-1 do
-    for x=0,shape.w-1 do
-      self[top+y][left+x] = shape[y*shape.w+x+1]
+--
+function _CELLS:spawn (shape, left, top)
+  for y = 0, shape.h - 1 do
+    for x = 0, shape.w - 1 do
+      self [top+y][left+x] = shape [y*shape.w+x+1]
     end
   end
 end
 
+--
 -- run the CA and produce the next generation
-function _CELLS:evolve(next)
-  local ym1,y,yp1,yi=self.h-1,self.h,1,self.h
+--
+function _CELLS:evolve (next)
+  local ym1, y, yp1, yi = self.h-1, self.h,1, self.h
+
   while yi > 0 do
-    local xm1,x,xp1,xi=self.w-1,self.w,1,self.w
+    local xm1, x, xp1, xi = self.w - 1, self.w, 1, self.w
     while xi > 0 do
-      local sum = self[ym1][xm1] + self[ym1][x] + self[ym1][xp1] +
-                  self[y][xm1] + self[y][xp1] +
-                  self[yp1][xm1] + self[yp1][x] + self[yp1][xp1]
-      next[y][x] = ((sum==2) and self[y][x]) or ((sum==3) and 1) or 0
-      xm1,x,xp1,xi = x,xp1,xp1+1,xi-1
+      local sum = self [ym1][xm1] + self [ym1][x] + self [ym1][xp1] +
+                  self [y][xm1]   + self [y][xp1] +
+                  self [yp1][xm1] + self [yp1][x] + self [yp1][xp1]
+      next [y][x] = ((sum == 2) and self [y][x]) or ((sum == 3) and 1) or 0
+      xm1, x, xp1, xi = x, xp1, xp1+1, xi - 1
     end
-    ym1,y,yp1,yi = y,yp1,yp1+1,yi-1
+    ym1, y, yp1, yi = y, yp1, yp1 + 1, yi-1
   end
 end
 
+--
 -- output the array to screen
+--
 function _CELLS:draw()
   local out = "" -- accumulate to reduce flicker
-  for y=1,self.h do
-   for x=1,self.w do
+  for y = 1, self.h do
+   for x = 1, self.w do
       out = out .. (((self[y][x] > 0) and ALIVE) or DEAD)
     end
     out = out.."\n"
   end
-  write(out)
+  write (out)
 end
 
+--
 -- constructor
-function CELLS(w,h)
-  local c = ARRAY2D(w,h)
-  c.spawn = _CELLS.spawn
+--
+function CELLS (w, h)
+  local c = ARRAY2D (w, h)
+
+  c.spawn  = _CELLS.spawn
   c.evolve = _CELLS.evolve
-  c.draw = _CELLS.draw
+  c.draw   = _CELLS.draw
   return c
 end
 
 --
 -- shapes suitable for use with spawn() above
 --
-HEART     = { 1,0,1,1,0,1,1,1,1; w=3,h=3 }
-GLIDER    = { 0,0,1,1,0,1,0,1,1; w=3,h=3 }
-EXPLODE   = { 0,1,0,1,1,1,1,0,1,0,1,0; w=3,h=4 }
-FISH      = { 0,1,1,1,1,1,0,0,0,1,0,0,0,0,1,1,0,0,1,0; w=5,h=4 }
-BUTTERFLY = { 1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,0,0,1; w=5,h=5 }
+HEART     = { 1,0,1,1,0,1,1,1,1;       w = 3,h = 3 }
+GLIDER    = { 0,0,1,1,0,1,0,1,1;       w = 3,h = 3 }
+EXPLODE   = { 0,1,0,1,1,1,1,0,1,0,1,0; w = 3, h = 4 }
+FISH      = { 0,1,1,1,1,1,0,0,0,1,0,0,0,0,1,1,0,0,1,0; w = 5, h = 4 }
+BUTTERFLY = { 1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,0,0,1; w = 5, h = 5 }
 
+--
 -- the main routine
-function LIFE(w,h)
+--
+function LIFE (w, h)
   -- create two arrays
-  local thisgen = CELLS(w,h)
-  local nextgen = CELLS(w,h)
+  local thisgen = CELLS (w, h)
+  local nextgen = CELLS (w, h)
 
   -- create some life
   -- about MAX_GEN/2 generations of fun, then a glider steady-state
-  thisgen:spawn (GLIDER,5,4)
-  thisgen:spawn (EXPLODE,25,10)
-  thisgen:spawn (FISH,4,12)
+  thisgen:spawn (GLIDER, 5, 4)
+  thisgen:spawn (EXPLODE, 25, 10)
+  thisgen:spawn (FISH, 4, 12)
 
   -- run until break
   local gen = 1
   write("\027[2J")	-- ANSI clear screen
   while 1 do
-    thisgen:evolve(nextgen)
-    thisgen,nextgen = nextgen,thisgen
-    write("\027[H") -- ANSI home cursor
+    thisgen:evolve (nextgen)
+    thisgen,nextgen = nextgen, thisgen
+
+    write ("\027[H") -- ANSI home cursor
     thisgen:draw()
-    write("Life - generation ",gen,"\n")
+    write ("Life - generation ", gen, "\n")
     gen = gen + 1
-    if gen > MAX_GEN then break end
+    if gen > MAX_GEN then
+       break
+    end
     delay (100)
   end
 end
